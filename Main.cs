8598d7d70530004b53fd1e46fa915a61abc5069f
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-03-05 09:55:18 +0700

    Commit pertama

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Modul2_jurnal
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
